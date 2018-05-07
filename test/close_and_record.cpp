53c2563855133a2d335e2e11e0e73bc76da3c09c
George Jose Pollayil <gpollayil@gmail.com>
2018-05-07 11:50:29 +0200

    added the hand stopping when touch, like in IMUGraspController

--- test/close_and_record.cpp
+++ test/close_and_record.cpp
@@ -19,6 +19,8 @@
 #include <std_msgs/Float64MultiArray.h>
 #include <control_msgs/FollowJointTrajectoryAction.h>
 
+#include <std_msgs/Int8.h>
+
 // ROS CUSTOM MSGS
 #include <qb_interface/inertialSensor.h>
 #include <qb_interface/inertialSensorArray.h>
@@ -33,16 +35,24 @@
 #define CLOSE_TIME		4000			// Time in which the SoftHand closes totally
 #define N_WP_CLOSE		150 			// Number of trajectory points of the slow hand closing
 #define SKIP_TRAJ_DELAY	50              // Delay in ms to avoid "first trajectory before current time" in first hand close
-
+#define HAND_TIMEOUT    7.0             // Hand closing timeout after which, if no collision, go on
 
 #define RECORD_TOPIC_1	"/norm_publishing_topic"
 #define RECORD_TOPIC_2	"/qb_class_imu/acc"
 #define HAND_NAME		"right_hand"
 #define HAND_JOINT 		"right_hand_synergy_joint"
 
+
 // GLOBAL VARIABLES
+
 // Action client ptr for hand
 std::shared_ptr<actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction>> move_;
+
+// Subscriber for handle finger collision
+ros::Subscriber finger_col_sub;
+
+int touching_finger = 0;                // For gradual closing and arm stopping
+
 
 //-----------------------------------------------------------------------------------------------------------------------//
 // AUXILIARY FUNCTIONS
@@ -108,6 +118,11 @@ void sendHandTrajectory(double increment){
   	send_goal(increment);
 }
 
+/* ******************************************************************************************** */
+void fingerColCallback(const std_msgs::Int8::ConstPtr& msg){
+    touching_finger = msg->data;
+}
+
 //-----------------------------------------------------------------------------------------------------------------------//
 // MAIN
 //-----------------------------------------------------------------------------------------------------------------------//
@@ -120,13 +135,37 @@ int main(int argc, char** argv){
 	// Initializing action client for hand
 	move_ = std::make_shared<actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction>>("/" + std::string(HAND_NAME) + "/joint_trajectory_controller/follow_joint_trajectory", true);
 
+    // Subscribing to handle finger collision
+    finger_col_sub = cr_nh.subscribe("/touching_finger_topic", 1000, fingerColCallback);
+
 	// Closing hand slowly
 	sendHandTrajectory(double (1.0));
 
     // sleep(5);
 
     // FOR TRYING TO CANCEL GOAL
-    // move_->cancelGoal();
+
+    // Starting to measure time
+    ros::Time before_time = ros::Time::now();
+    ros::Duration timeout(HAND_TIMEOUT);
+
+    bool first_print_out = true;
+    ROS_INFO("I'm waiting for a finger collision!\n");
+
+    while(touching_finger == 0 && ros::Time::now() - before_time < timeout){
+
+        ros::spinOnce();
+        
+        if (!touching_finger) {
+            if(first_print_out){
+                ROS_INFO("No finger collision received yet!\n");
+                first_print_out = false;
+            }
+        } else {
+            ROS_INFO("A COLLISION DETECTED ON FINGER %d!\n", touching_finger);
+            move_->cancelGoal();
+        }
+    }
 
 	// While closing, record bag file
 	ROS_INFO_STREAM("Starting to record the bag! \n");
@@ -134,7 +173,12 @@ int main(int argc, char** argv){
 		std::cout << "The hand is not closing properly!" << std::endl;
 	};
 
-    sleep(5);
+    sleep(1);
+
+    // Closing hand completely
+    sendHandTrajectory(double (1.0));
+
+    sleep(3);
 
 	// Opening hand
 	sendHandTrajectory(double (0.0));
